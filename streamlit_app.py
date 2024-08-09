8f4de00a777431857b7f7ea337a2407e718aa868
swatiraut2990 <swati.raut2990@gmail.com>
2024-08-10 01:08:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information'
+        st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         #st.text(fruityvice_response.json())
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
@@ -48,5 +48,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect() 
         st.success('Your Smoothie is ordered, '+name_on_order+'!',icon="✅")
-
-    
